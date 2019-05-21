8cc672505603f33c26c10c72a804c63c0f5e54f6
Jan Vansteenkiste <jan@vstone.eu>
2019-05-21 11:13:40 +0200

    Introduce new way to manage the data + scripts

--- convert.rb
+++ convert.rb
@@ -2,5 +2,14 @@
 
 require 'yaml'
 require 'json'
+require 'awesome_print'
 
-File.write('data.json', YAML.load_file('data.yaml').to_json())
+#File.write('data.json', YAML.load_file('data.yaml').to_json())
+all_data = YAML.load_file('data.yaml')
+
+all_data.each do |week, config|
+  File.write("data/#{week}.json", config.to_json())
+end
+
+week_map = Hash[all_data.map { |week, data| [ week, data['singe'] ] }]
+File.write("weeks.json", week_map.to_json())
