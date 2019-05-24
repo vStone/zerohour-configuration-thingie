7d1c34ab994555cddce55509bc24f96da4df7100
Jan Vansteenkiste <jan@vstone.eu>
2019-05-24 07:36:36 +0200

    Converted the data to a rotation based system

--- convert.rb
+++ convert.rb
@@ -7,9 +7,9 @@ require 'awesome_print'
 #File.write('data.json', YAML.load_file('data.yaml').to_json())
 all_data = YAML.load_file('data.yaml')
 
-all_data.each do |week, config|
-  File.write("data/#{week}.json", config.to_json())
+Dir.mkdir('data') unless File.exist?('data')
+all_data['configurations'].each do |singe, config|
+  File.write("data/#{singe}.json", config.to_json())
 end
 
-week_map = Hash[all_data.map { |week, data| [ week, data['singe'] ] }]
-File.write("weeks.json", week_map.to_json())
+File.write("rotations.json", all_data['rotations'].to_json())
